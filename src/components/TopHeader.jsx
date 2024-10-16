939ceea456c0dab15dee1be97a4a705ee9a2452a
Jota Cê <100882192+joicyCarolaine@users.noreply.github.com>
2024-10-16 20:03:53 -0300

    Update TopHeader.jsx

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -3,7 +3,7 @@ export default function topHeader() {
     <header className=" bg-black p-2 font-thin text-white text-center">
       <span className="">Use o cupom </span>
       <span className="mr-1 font-bold">
-        <a href="https://www.linkedin.com/in/moemenadam/" target="_blank">Xoices</a>
+        <a href="" target="_blank">Xoices</a>
       </span>
       <span>para obter 20% de desconto</span>
     </header>
